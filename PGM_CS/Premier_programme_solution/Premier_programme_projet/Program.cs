b8e75a9371467c294fcce94b10b4fc1eb9e124c8
jamlocal <asp.jam@outlook.com>
2021-03-24 07:55:13 +0100

    Création du depot C#

---
+++ PGM_CS/Premier_programme_solution/Premier_programme_projet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Premier_programme_projet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*string nom = "Joey";
+            Console.WriteLine("Entrez votre nom : ");
+            nom = Console.ReadLine();
+            Console.WriteLine("Votre nom est  " + nom);
+            Console.ReadLine();*/
+
+            string age;
+            Console.Write("Qu'elle est votre age : ");
+            age = Console.ReadLine();
+            Console.WriteLine("Bonjour vous avez " + age + "ans");
+
+
+
+
+
+
+
+        }
+    }
+}
