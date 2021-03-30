19481fe6241e3639fe5de735fe6a12f19b3323d0
jam <jajam777@outlook.com>
2021-03-30 18:55:27 +0200

    Terminé methode et surcharge et commencé Exception

--- PGM_CS/Methode_Exception/Exception/Program.cs
+++ PGM_CS/Methode_Exception/Exception/Program.cs
@@ -1,12 +1,132 @@
 ﻿using System;
 
-namespace Exception
+namespace Exception1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Exception
+
+            /*
+             * 
+             * une exception est une erreur produite durant l'exécution de votre programme. Sémantiquement parlant, le terme d'exception a été retenue car une erreur est censée être exceptionnelle : c'est n'est pas le mode de fonctionnement nominale de votre application.
+             * 
+             * La chose la plus importante à comprendre c'est qu'en cas de déclenchement d'exception (d'erreur) le programme passe dans un mode d'exécution particulier : il faut qu'il trouve un gestionnaire d'erreur (un bloc d'instruction try / catch).
+             * 
+             * .NET Framework fournit plusieurs classes d'exception
+             * 
+             * Exception
+             * SystemException
+             * ApplicationException
+             * NullReferenceException
+             * FileNotFoundException
+             * SerializationException
+             * 
+             * Une exception est l'indication d'une erreur ou d'une condition exceptionnelle. 
+             * Une méthode peut lever une exception lorsqu'elle détecte qu'un événement inattendu s'est produit, par exemple, l'application tente d'ouvrir un fichier qui n'existe pas.
+             * 
+             * 
+             * Try{ code appelant } catch(xxxException 1)  { code gestion exception 1 } ... catch(xxxException n){ code gestion exception n }
+             * Si la methode leve un exception dans tous les cas le code appelant est abandonné
+             * La methode recherche l'exception dans les catch et si il a la trouve execute le code de gestion
+             * 
+             * Et si aucun code ne gere cette exception le processus s'arrete et affiche un mesaage d'eerue
+             * 
+             * Toutes les exceptions heritent de la classe Exception
+             * 
+             * Lorsqu'une méthode lève une exception, elle crée un objet Exception et peut le renseigner avec des informations sur la cause de l'erreur. L'objet Exception est ensuite passé au code qui gère l'exception.
+             * 
+             * On place toujours la classe d'exception ciblée en premier et la générale en dernier en l'occurence Exeption
+             * 
+             */
+
+            int[] values = null;
+
+            //Unhandled exception. System.NullReferenceException: 
+            //Object reference not set to an instance of an object.
+            //Console.WriteLine(values[0]);
+
+            /*try
+            {
+                Console.WriteLine("Code appelant dans Try avant1");//EXECUTE
+                Console.WriteLine("Code appelant dans Try avant2");//EXECUTE
+                Console.WriteLine("values[0] = " + values[0]);//PAS EXECUTE
+                Console.WriteLine("Code appelant dans Try apres1");// PAS EXECUTE
+                Console.WriteLine("Code appelant dans Try apres2");// PAS EXECUTE
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Data " + e.Data);//System.Collections.ListDictionaryInternal
+                Console.WriteLine("InnerException " + e.InnerException);//null
+                Console.WriteLine("Message " + e.Message);//Object reference not set to an instance of an object.
+                Console.WriteLine("Source " + e.Source);//Source Exception
+                Console.WriteLine("TargetSite " + e.TargetSite);//TargetSite Void Main(System.String[])
+
+            }
+
+            Console.WriteLine("Apres le catch");//EXECUTE*/
+
+            /*try
+            {
+                Console.WriteLine("Code appelant dans Try avant1");//EXECUTE
+                Console.WriteLine("Code appelant dans Try avant2");//EXECUTE
+                Console.WriteLine("values[0] = " + values[0]);//PAS EXECUTE
+                Console.WriteLine("Code appelant dans Try apres1");// PAS EXECUTE
+                Console.WriteLine("Code appelant dans Try apres2");// PAS EXECUTE
+
+            }
+            catch (NullReferenceException e) //Capturé ici CODE EXECUTE
+            {
+
+                Console.WriteLine("\n\nNullReferenceException");
+                Console.WriteLine("Data " + e.Data);//System.Collections.ListDictionaryInternal
+                Console.WriteLine("InnerException " + e.InnerException.Message);//NullReferenceException
+                Console.WriteLine("Message " + e.Message);//Object reference not set to an instance of an object.
+                Console.WriteLine("Source " + e.Source);//Source Exception
+                Console.WriteLine("TargetSite " + e.TargetSite);//TargetSite Void Main(System.String[])
+
+            }
+            catch (Exception e)//Pas capturé car capturé avant par NullReferenceException et code NON EXECUTE
+            {
+                Console.WriteLine("\n\nException");
+                Console.WriteLine("Data " + e.Data);//System.Collections.ListDictionaryInternal
+                Console.WriteLine("InnerException " + e.InnerException.Message);//rien
+                Console.WriteLine("Message " + e.Message);//Object reference not set to an instance of an object.
+                Console.WriteLine("Source " + e.Source);//Source Exception
+                Console.WriteLine("TargetSite " + e.TargetSite);//TargetSite Void Main(System.String[])
+
+
+            }
+
+            Console.WriteLine("Apres le catch");//EXECUTE*/
+
+
+            //Division par zéro
+
+
+            try
+            {
+
+                Console.Write("\n\nEntrez un nombre : ");
+                int n = int.Parse(Console.ReadLine());//Si n=0 PAS EXECUTE et tout ce qui suit
+                Console.WriteLine("100/nombre = {0}", 100 / n);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Entré dans Catch Exception = " + e.Message);
+                //throw;
+                Console.WriteLine("Source " + e.Source);//Source Exception
+            }
+
+            Console.WriteLine("Apres catch");//EXECUTE
+
+            Console.Write("\n\nEntrez un nombre : ");
+            int n1 = int.Parse(Console.ReadLine());//Si n=0 PAS EXECUTE et tout ce qui suit
+            Console.WriteLine("100/nombre = {0}", 100 / n1);
+
+
         }
     }
 }
--- PGM_CS/Methode_Exception/Methode_Exception/Program.cs
+++ PGM_CS/Methode_Exception/Methode_Exception/Program.cs
@@ -92,20 +92,128 @@ namespace Methode_Exception
             //Même nom
             //Signature unique donc parametres differents
 
-            Sur();
-            Sur(51);
+            /*Attention ici la methode main() est static , on dit que c'est une methode de classe 
+             
+            Une méthode d'instance peut utiliser un membre de classe (attribut static ou méthode static).
+            Une méthode de classe (methode static) peut utiliser un membre de classe (attribut static ou méthode static)..
+            Une méthode de classe ne peut pas utiliser un membre d'instance (attribut ou méthode).
+            Une méthode de classe ne peut pas utiliser le mot-clé this.
+            
+            Les methodes surchargées ne peuvent se faire que dans la classe et pas dans la methode
+            Commme main est static elle ne peut utiliser que des methodes static
+            C'est pour quoi on a declaré static void Sur() en dehors de la methode et ajoute static
 
+             */
 
+            Sur(12, true);//"Original a=12 et b=true"
+            Sur(false, 12);//Original a=12 et b=False //Inversion des parametres
+            Sur(12, true, 50);//Original a=12 et b=True c= 50 //Ajout d'un parametre
 
+            //Parametre facultatif
 
+            /*
+             Ils fournissent une solution compacte et simple lorsqu'il n'est pas possible d'utiliser surcharge car les types de paramètres ne varient pas suffisamment pour permettre au compilateur de distinguer les implémentations.
+             */
+
+            void fac(int a, string m, int b = 45)
+            {
+                Console.WriteLine("a = " + a + " m = " + m + " b = " + b);
+            }
+
+            fac(12, "bonjour");//a = 12 m = bonjour b = 45
+            fac(12, "bonjour", 150);//a = 12 m = bonjour b = 150
+                                    //fac(12, 150, "bonjour");//Mauvais positionnement des parametres
+
+            //Specifier tous les parametres obligatoire avant les parametres facultatif
+
+            //Erreur parametres facultatif apres parametres requis
+            /* void fac2(int a, string m="defaut", int b )
+             {
+                 Console.WriteLine("a = " + a + " m = " + m + " b = " + b);
+             }*/
+
+            //Arguments nommés (Named arguments)
+
+            /*
+             lors de l'appel d'une méthode, l'ordre et la position des arguments dans l'appel de méthode correspondent à l'ordre des paramètres dans la signature de la méthode. Si les arguments ne sont pas alignés et que les types ne correspondent pas, vous recevez une erreur de compilation.
+             
+             */
+
+            void named(int a, string m, int b)
+            {
+                Console.WriteLine("a = " + a + " m = " + m + " b = " + b);
+            }
+
+            named(m: "bonjour", a: 55, b: 88);//a = 55 m = bonjour b = 88
+
+            //named(55,b: 88,"bonjour");//Erreur b hors position
+
+            void named2(int a, int b, string m = "bonjour")
+            {
+                Console.WriteLine("a = " + a + " m = " + m + " b = " + b);
+            }
+
+            named2(b: 100, a: 200);//a = 200 m = bonjour b = 100
+
+            //Parametre de sortie output parameter
+
+            void named3(out string message, out int b)
+            {
+                message = "Message"; //Obliger d'assigner message sinon erreur
+                b = 50; // Obliger d'assigner b sinon erreur
+
+            }
+
+            string mes;
+            int f = 10;
+            named3(out mes, out f);
+            Console.WriteLine(mes);//"Message"
+            Console.WriteLine(f);//50
+
+            named3(out string mes2, out int f2);
+            Console.WriteLine("mes2=" + mes2);//"Message"
+            Console.WriteLine("f2=" + f2);//50
+
+            //On peut pas mettre parametre faculatatif avant parametre de sortie
+            int named4(int a, out string message, out int b, int c = 100)
+            {
+                message = "Message suivant"; //Obliger d'assigner message sinon erreur
+                b = 300; // Obliger d'assigner b sinon erreur
+                return a + c;
+
+            }
+
+            Console.WriteLine(named4(100, out mes, out f));//200
+            Console.WriteLine(mes);//"Message suivant"
+            Console.WriteLine(f);//300
 
 
         }
-        static void Sur() { Console.WriteLine("Original"); }
 
-        //Changer le type de retour      
+        static void Sur(int a, bool b) { Console.WriteLine("Original a=" + a + " et b=" + b); }
+
+        //Changer le type de retour : PAS DE SURCHARGE
+
+        //static int Sur(int a,bool b) { return a; }//Erreur pas de surcharge
+
+        //Inverser les parametres
+        static void Sur(bool b, int a) { Console.WriteLine("Original a=" + a + " et b=" + b); }
+
+        //Ajouter un parametre
+        static void Sur(int a, bool b, int c) { Console.WriteLine("Original a=" + a + " et b=" + b + " c= " + c); }
+
+        //Supprimer des parametres
+        static void Sur() { Console.WriteLine("Aucun ârametre"); }
+
+        //Modifier le corps de la methode : PAS DE SURCHARGE
+
+        //static void Sur(int a, bool b) { Console.WriteLine("Corps modifié"); }//Erreur methode existe deja
+
+        //Modifier la visibilité : PAS DE SURCHARGE
+        //private static void Sur(int a, bool b) { Console.WriteLine("Corps modifié"); }
+        //public static void Sur(int a, bool b) { Console.WriteLine("Corps modifié"); }
 
 
-        static void Sur(int d) { Console.WriteLine("Surcharge" + d); }
+
     }
 }
