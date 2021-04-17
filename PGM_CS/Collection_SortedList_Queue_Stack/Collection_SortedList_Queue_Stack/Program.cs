fe1a9b5cbda56e1136aad7ca16746f170e421cfc
jamlocal <asp.jam@outlook.com>
2021-04-17 09:14:15 +0200

    Collection non generique terminée

---
+++ PGM_CS/Collection_SortedList_Queue_Stack/Collection_SortedList_Queue_Stack/Program.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+
+namespace Collection_SortedList_Queue_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SortedList
+            //Implémente ICollection IDictionary  IEnumerable ICloneable
+
+            //Constructeurs
+
+            /*             
+            SortedList()
+            Initialise une nouvelle instance de la classe SortedList 
+            qui est vide, possède la capacité initiale par défaut et 
+            est triée suivant l'interface IComparable implémentée par 
+            chaque clé ajoutée à l'objet SortedList.
+            */
+
+            SortedList Slist1_num = new SortedList();
+            SortedList Slist1_string = new SortedList();
+            SortedList Slist1_obj = new SortedList();
+            SortedList Slist1_tout = new SortedList();
+
+            Slist1_num.Add(10, 12);
+            Slist1_num.Add(1, 55);
+            Slist1_num.Add(-5, 2);
+
+            ShowList(Slist1_num);
+
+            /* Trier par ordre croissant des clés
+            -5 , 2
+            1 , 55
+            10 , 12             
+             */
+            //On ne peut pas avoir 2 clé identiques
+            Slist1_string.Add("zgh", 12);
+            Slist1_string.Add("cgh", 55);
+            Slist1_string.Add("Zgh", 2);
+            Slist1_string.Add("Art", 2);
+            Slist1_string.Add("0rt", 2);
+            Slist1_string.Add("5rt", 2);
+
+            Console.WriteLine();
+            ShowList(Slist1_string);
+
+            /* Trier par ordre croissant des clés
+            0rt , 2
+            5rt , 2
+            Art , 2
+            cgh , 55
+            zgh , 12
+            Zgh , 2            
+             */
+
+            /*Slist1_obj.Add(new Produit(120), 12);
+            Slist1_obj.Add(new Produit(14), 55);
+            Slist1_obj.Add(new Produit(-9), 2);*/
+            //System.InvalidOperationException:
+            //Failed to compare two elements in the array.
+
+            /*
+            SortedList(IComparer)
+            public SortedList (System.Collections.IComparer? comparer);
+
+            public interface IComparer
+            MÉTHODES
+            Compare(Object, Object)
+
+            Initialise une nouvelle instance de la classe SortedList
+            qui est vide, possède la capacité initiale par défaut et 
+            est triée suivant l'interface IComparer spécifiée.              
+            */
+            Comparer myCompIntl = new Comparer(new CultureInfo("fr-FR"));
+            Console.WriteLine("Compare : " + myCompIntl.Compare("a", "b"));//-1
+            Console.WriteLine("Compare : " + myCompIntl.Compare("b", "b"));//0
+            Console.WriteLine("Compare : " + myCompIntl.Compare("b", "a"));//1
+
+            Console.WriteLine("Compare : " + myCompIntl.Compare(12, 15));//-1
+            Console.WriteLine("Compare : " + myCompIntl.Compare(13, 13));//0
+            Console.WriteLine("Compare : " + myCompIntl.Compare(100.23, 0.123));//1
+
+
+            SortedList Slist1_obj1 = new SortedList(new Compare_Produit());
+            Slist1_obj1.Add(new Produit(120), 12);
+            Slist1_obj1.Add(new Produit(14), 55);
+            Slist1_obj1.Add(new Produit(-9), 2);
+
+            Console.WriteLine();
+            ShowList(Slist1_obj1);
+
+
+            /*
+            SortedList(IDictionary)
+            Initialise une nouvelle instance de la classe SortedList 
+            qui contient des éléments copiés à partir du dictionnaire 
+            spécifié, possède la capacité initiale correspondant au
+            nombre d'éléments copiés et est triée suivant l'interface 
+            IComparable implémentée par chaque clé. 
+
+            
+             
+            */
+
+            Hashtable hash = new Hashtable();
+            hash.Add("hash", 1000);
+            hash.Add("hash2", 2000);
+            hash.Add("hash3", 3000);
+
+            SortedList l1 = new SortedList(hash);
+            Console.WriteLine();
+            ShowList(l1);
+            /*
+            hash , 12
+            hash2 , 55
+            hash3 , 2 
+            */
+
+            SortedList l2 = new SortedList(15);
+            l2.Add("zgh", 12);
+            l2.Add("cgh", 55);
+            Console.WriteLine();
+            ShowList(l2);
+
+            Console.WriteLine(l1.Count);//3
+            Console.WriteLine(l1.Capacity);//3
+
+            Console.WriteLine(l2.Count);//2
+            Console.WriteLine(l2.Capacity);//15
+
+            Console.WriteLine(l1["hash"]);//1000
+            l1.Keys;
+            l1.Values;
+
+            /*
+             MÉTHODES
+                Add(Object, Object)
+                Ajoute un élément avec la clé et la valeur spécifiées dans un objet SortedList.
+                
+                Clear()
+                Supprime tous les éléments d'un objet SortedList.
+                
+                Clone()
+                Crée une copie superficielle d'un objet SortedList.
+
+                Contains(Object)
+                Détermine si un objet SortedList contient une clé spécifique.
+
+                ContainsKey(Object)
+                Détermine si un objet SortedList contient une clé spécifique.
+
+                ContainsValue(Object)
+                Détermine si un objet SortedList contient une valeur spécifique.                
+                
+                GetByIndex(Int32)
+                Obtient la valeur à l'index spécifié d'un objet SortedList.
+                
+                GetKey(Int32)
+                Obtient la clé à l'index spécifié d'un objet SortedList.
+
+                GetKeyList()
+                Obtient les clés d'un objet SortedList.
+               
+                GetValueList()
+                Obtient les valeurs d'un objet SortedList.
+
+                IndexOfKey(Object)
+                Retourne l'index de base zéro de la clé spécifiée dans un objet SortedList.
+
+                IndexOfValue(Object)
+                Retourne l'index de base zéro de la première occurrence de la valeur spécifiée dans un objet SortedList.
+
+                
+                Remove(Object)
+                Supprime d'un objet SortedList l'élément ayant la clé spécifiée.
+
+                RemoveAt(Int32)
+                Supprime l'élément à l'index spécifié d'un objet SortedList.
+                SetByIndex(Int32, Object)
+                Remplace la valeur à l'index spécifié dans un objet SortedList. 
+             
+             */
+
+            void ShowList(SortedList collection)
+            {
+                foreach (DictionaryEntry item in collection)
+                {
+                    if (item.Key is Produit)
+                    {
+                        Produit prix = (Produit)item.Key;
+                        Console.WriteLine(prix.Prix + " , " + item.Value);
+                    }
+                    else
+                    {
+                        Console.WriteLine(item.Key + " , " + item.Value);
+                    }
+
+                }
+            }
+
+        }
+    }
+
+    class Produit
+    {
+        public Produit(int prix)
+        {
+            Prix = prix;
+        }
+
+        public int Prix { get; set; }
+        public string Marque = "Pomme";
+
+
+
+    }
+    class Compare_Produit : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            //Compare deux objets et retourne une valeur indiquant
+            //si le premier est inférieur, égal ou supérieur au second.
+
+            //Quand sort est appelé il compare 2 a 2 les clés qui sont des produits
+            //Object x=Produit px_key passage par reference
+            //Object y=Produit py_key passage par reference
+
+            Produit px = (Produit)x;
+            Produit py = (Produit)y;
+
+            var prix_x = px.Prix;
+            var prix_y = py.Prix;
+
+            return prix_y.CompareTo(prix_x);
+        }
+    }
+}
---
+++ PGM_CS/Stack_Queue/Stack_Queue/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+
+namespace Stack_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stack Représente une simple collection non générique
+            //d’objets de type dernier entré, premier sorti (LIFO).
+
+            /*
+             Implémente : ICollection  IEnumerable  ICloneable
+             
+             CONSTRUCTEURS
+                Stack()	
+                Stack(ICollection)	
+                Stack(Int32)
+
+            Propriété Count
+
+           
+            MÉTHODES
+            Clear()	
+            Supprime tous les objets du Stack.
+
+            Clone()	
+            Crée une copie superficielle de Stack.
+
+            Contains(Object)	
+            Détermine si le Stack contient un élément.
+
+            Pop()	
+            Supprime et retourne l’objet en haut de Stack.
+
+            Push(Object)	
+            Insère un objet en haut de Stack.  
+            
+            ToArray()	
+            Copie les éléments Queue vers un nouveau tableau.
+             */
+
+            //Stack()
+            int i = 50;
+            Produit p = new Produit(120);
+            string[] mot = { "pomme", "poire" };
+            bool t = true;
+
+            Stack s1 = new Stack();
+            s1.Push(i);
+            Show(s1);//50
+            s1.Push(t);
+            Show(s1);//true 50
+            s1.Push(p);
+            Show(s1);//Stack_Queue.Produit True 50
+
+            ArrayList l = new ArrayList();
+
+            var t3 = s1.ToArray();
+            Console.WriteLine();
+            Show(t3);
+            Console.WriteLine(t3[2]);//50
+            Console.WriteLine(((Produit)t3[0]).Prix);//120
+
+            s1.Pop();
+            Console.WriteLine();
+            Show(s1);//True 50
+
+            /*
+             Le Premier Element du stack est en fait le dernier entrer
+            Les elements sont insérer par le debut de la collection
+             */
+
+            var f = s1.Peek();
+            Console.WriteLine();
+            Console.WriteLine(f);//True retourne le dernier element rentrer en haut de la pile
+                                 //Sans rien supprimer
+            Console.WriteLine();
+            Show(s1);//True 50
+
+            Console.WriteLine(/****Stack****/);
+            Stack s3 = new Stack();
+            for (int i1 = 1; i1 <= 3; i1++)
+            {
+                s3.Push(i1);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine(/****Queue****/);
+            Show(s3);//1 2 3
+                     //Premier entrer a indice 0
+                     //Deuxieme a indice 1
+                     //Ici pas de decallage
+
+
+
+
+            //Queue Représente une collection d’objets premier entré, premier sorti.
+            //FiFO First input first output
+            /*
+             //Constructeur comme stack
+
+            //Count
+
+            //Methodes idem que Stack sauf
+
+            Dequeue()	
+            Supprime et renvoie l'objet au début de la Queue.
+
+            Enqueue(Object)	
+            Ajoute un objet à la fin de la Queue.
+             
+             */
+
+            Queue q = new Queue();
+            for (int i1 = 1; i1 <= 3; i1++)
+            {
+                q.Enqueue(i1);
+            }
+            Console.WriteLine();
+            Console.WriteLine(/****Queue****/);
+            Show(q);//1 2 3
+
+            Console.WriteLine();
+            q.Dequeue();
+            Show(q);//2 3
+
+            Console.WriteLine();
+            var g = q.Peek();
+            Console.WriteLine(g);//2
+
+            Console.WriteLine();
+            Show(q);//2 3
+
+            void Show(ICollection collection)
+            {
+                foreach (var item in collection)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+        }
+    }
+
+    class Produit
+    {
+
+
+        public Produit(int prix)
+        {
+            Prix = prix;
+        }
+
+        public int Prix { get; set; }
+
+
+    }
+
+
+}
